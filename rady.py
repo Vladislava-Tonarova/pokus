8969fe6355cf20cf371c35b9c03d2239a04f0769
Vladislava Tonarova <tonarovv@seznam.cz>
2023-10-26 15:25:01 +0200

    přidán poslední řádek

--- rady.py
+++ rady.py
@@ -1,8 +1,8 @@
-#tento program rozdává rady do života
+# tento program rozdává rady do života
 
 print("Odpovídej ano nebo ne")
 stastna_retezec = input("jsi šťastná?")
-if stastna_retezec == "ano" or stastna_retezec ==  "Ano":
+if stastna_retezec == "ano" or stastna_retezec == "Ano":
     stastna = True
 elif stastna_retezec == "ne" or stastna_retezec == "Ne":
     stastna = False
@@ -25,3 +25,5 @@ elif stastna:
     print("Zkus míň utrácet!")
 else:
     print("To je mi líto!")
+
+    print("hello world!")
